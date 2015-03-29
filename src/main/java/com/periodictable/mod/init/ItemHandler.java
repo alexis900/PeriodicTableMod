dfc23fa0d77222a46eccdbfd2734a4dc3f3910f5
alexis900 <alexandermp9@hotmail.com>
2015-03-29 19:20:25 +0200

    Carbon

--- src/main/java/com/periodictable/mod/init/ItemHandler.java
+++ src/main/java/com/periodictable/mod/init/ItemHandler.java
@@ -15,6 +15,7 @@ public class ItemHandler {
 	public static Item Lithium;
 	public static Item Beryllium;
 	public static Item Boron;
+	public static Item Carbon;
 	
 	public static void init(){
 		Hydrogen = new Item().setUnlocalizedName("Hydrogen").setCreativeTab(Main.tabPeriodic);
@@ -22,6 +23,7 @@ public class ItemHandler {
 		Lithium = new Item().setUnlocalizedName("Lithium").setCreativeTab(Main.tabPeriodic);
 		Beryllium = new Item().setUnlocalizedName("Beryllium").setCreativeTab(Main.tabPeriodic);
 		Boron = new Item().setUnlocalizedName("Boron").setCreativeTab(Main.tabPeriodic);
+		Carbon = new Item().setUnlocalizedName("Carbon").setCreativeTab(Main.tabPeriodic);
 	}
 public static void register(){
 	GameRegistry.registerItem(Hydrogen, Hydrogen.getUnlocalizedName().substring(5));
@@ -29,6 +31,7 @@ public static void register(){
 	GameRegistry.registerItem(Lithium, Lithium.getUnlocalizedName().substring(5));
 	GameRegistry.registerItem(Beryllium, Beryllium.getUnlocalizedName().substring(5));
 	GameRegistry.registerItem(Boron, Boron.getUnlocalizedName().substring(5));
+	GameRegistry.registerItem(Carbon, Carbon.getUnlocalizedName().substring(5));
 }
 public static void registerRenders(){
 	registerRender(Hydrogen);
@@ -36,6 +39,7 @@ public static void registerRenders(){
 	registerRender(Lithium);
 	registerRender(Beryllium);
 	registerRender(Boron);
+	registerRender(Carbon);
 }
 public static void registerRender(Item item){
 	Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item	, 0, new ModelResourceLocation(Reference.MOD_ID + ":" + item.getUnlocalizedName().substring(5),"inventory"));
